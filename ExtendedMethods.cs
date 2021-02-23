af1e0d3eddfc12c6ae46e3d2c2c62ee407b816e1
Skyzi000 <skyzi000r@gmail.com>
2021-02-24 05:40:41 +0900

    拡張メソッド用のクラスを追加して、Streamを圧縮/展開しながらコピーできるメソッドを追加した

---
+++ ExtendedMethods.cs
@@ -0,0 +1,57 @@
+﻿using Skyzi000.Cryptography;
+using System.IO;
+using System.IO.Compression;
+namespace Skyzi000
+{
+    public static class ExtendedMethods
+    {
+        /// <summary>
+        /// <see cref="Stream"/>を圧縮/展開しながらコピーします。
+        /// </summary>
+        public static void CopyWithCompressionTo(this Stream stream, Stream destination, CompressionLevel compressionLevel, CompressionMode compressionMode, CompressAlgorithm compressAlgorithm = CompressAlgorithm.Deflate) 
+        {
+            if (compressionLevel == CompressionLevel.NoCompression)
+            {
+                stream.CopyTo(destination);
+            }
+            else if(compressionMode == CompressionMode.Decompress)
+            {
+                switch (compressAlgorithm)
+                {
+                    case CompressAlgorithm.Deflate:
+                        {
+                            using DeflateStream compressionStream = new DeflateStream(stream, CompressionMode.Decompress);
+                            compressionStream.CopyTo(destination);
+                            break;
+                        }
+
+                    case CompressAlgorithm.GZip:
+                        {
+                            using GZipStream compressionStream = new GZipStream(stream, CompressionMode.Decompress);
+                            compressionStream.CopyTo(destination);
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                switch (compressAlgorithm)
+                {
+                    case CompressAlgorithm.Deflate:
+                        {
+                            using DeflateStream compressionStream = new DeflateStream(destination, compressionLevel);
+                            stream.CopyTo(compressionStream);
+                            break;
+                        }
+
+                    case CompressAlgorithm.GZip:
+                        {
+                            using GZipStream compressionStream = new GZipStream(destination, compressionLevel);
+                            stream.CopyTo(compressionStream);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
