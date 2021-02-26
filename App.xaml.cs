2f6e3d693e31d70e62605e77d9a9f3d6ad5058b9
Skyzi000 <skyzi000r@gmail.com>
2021-02-27 04:16:11 +0900

    Formsへの参照を追加し、通知アイコンを表示するようにした。MainWindowを自動的に起動しないようにした。

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,15 +3,67 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace SkyziBackup
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        public static NotifyIcon NotifyIcon { get; private set; }
+        public static AssemblyName AssemblyName = Assembly.GetExecutingAssembly().GetName();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("SkyziBackup.ico", UriKind.Relative)).Stream;
+            var menu = new ContextMenuStrip();
+            menu.Items.Add("メイン画面を表示する", null, MainShow_Click);
+            menu.Items.Add("終了する", null, Exit_Click);
+            NotifyIcon = new NotifyIcon
+            {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = AssemblyName.Name,
+                ContextMenuStrip = menu
+            };
+            NotifyIcon.MouseClick += new MouseEventHandler(NotifyIcon_Click);
+        }
+        private void ShowMainWindowIfClosed()
+        {
+            if (MainWindow == null)
+            {
+                MainWindow = new MainWindow();
+                MainWindow.Closed += (s, e) =>
+                {
+                    NotifyIcon.ShowBalloonTip(10000, $"{AssemblyName.Name}", "終了するには通知アイコンを右クリックしてください。", ToolTipIcon.Info);
+                    MainWindow = null;
+                };
+                MainWindow.Show();
+            }
+            MainWindow.Activate();
+        }
+        private void MainShow_Click(object sender, EventArgs e)
+        {
+            ShowMainWindowIfClosed();
+        }
+        private void NotifyIcon_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ShowMainWindowIfClosed();
+            }
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // TODO: バックアップ中は確認する
+            NotifyIcon.Visible = false;
+            Shutdown();
+        }
     }
 }
