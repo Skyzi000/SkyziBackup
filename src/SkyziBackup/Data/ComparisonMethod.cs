fe11bfe8ca92bc11d40a1dbcccc1aa800d685960
Skyzi000 <skyzi000r@gmail.com>
2022-04-17 11:35:54 +0900

    設定関連のenumを各ファイルに分割

---
+++ src/SkyziBackup/Data/ComparisonMethod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SkyziBackup.Data
+{
+    /// <summary>
+    /// ファイルの変更を検知する方法
+    /// </summary>
+    [Flags]
+    public enum ComparisonMethod
+    {
+        /// <summary>
+        /// 比較しない
+        /// </summary>
+        /// <remarks>他の値と同時に指定することはできない</remarks>
+        NoComparison = 0,
+
+        /// <summary>
+        /// Archive属性による比較
+        /// </summary>
+        /// <remarks>バックアップ時に元ファイルのArchive属性を変更する点に注意</remarks>
+        ArchiveAttribute = 1,
+
+        /// <summary>
+        /// 更新日時による比較
+        /// </summary>
+        WriteTime = 1 << 1,
+
+        /// <summary>
+        /// サイズによる比較
+        /// </summary>
+        Size = 1 << 2,
+
+        /// <summary>
+        /// SHA1による比較
+        /// </summary>
+        FileContentsSHA1 = 1 << 3,
+
+        /// <summary>
+        /// 生データによるバイナリ比較
+        /// </summary>
+        /// <remarks>データベースを利用出来ず、暗号化や圧縮と併用できない点に注意</remarks>
+        FileContentsBinary = 1 << 4,
+    }
+}
---
+++ src/SkyziBackup/Data/VersioningMethod.cs
@@ -0,0 +1,35 @@
+﻿namespace SkyziBackup.Data
+{
+    /// <summary>
+    /// 削除・上書き時の動作
+    /// </summary>
+    public enum VersioningMethod
+    {
+        /// <summary>
+        /// 完全消去する(バージョン管理を行わない)
+        /// </summary>
+        PermanentDeletion = 0,
+
+        /// <summary>
+        /// ゴミ箱に送る(ゴミ箱が利用できない時は完全消去する)
+        /// </summary>
+        RecycleBin = 1,
+
+        /// <summary>
+        /// 指定されたディレクトリにそのまま移動し、既存のファイルを置き換える
+        /// </summary>
+        Replace = 2,
+
+        /// <summary>
+        /// 新規作成されたタイムスタンプ付きのディレクトリ以下に移動する
+        /// <code>\YYYY-MM-DD_hhmmss\Directory\hoge.txt</code>
+        /// </summary>
+        DirectoryTimeStamp = 3,
+
+        /// <summary>
+        /// タイムスタンプを追加したファイル名で、指定されたディレクトリに移動する
+        /// <code>\Directory\File.txt_YYYY-MM-DD_hhmmss.txt</code>
+        /// </summary>
+        FileTimeStamp = 4,
+    }
+}
